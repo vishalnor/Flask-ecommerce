7ed2d28b0c3818dd721df88d0230edb8f2745d59
bhavinnor1 <bhavinnor13@gmail.com>
2023-07-15 11:17:59 +0530

    Added SQLAlchemy

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask,render_template
+# from models import Item
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password123@localhost/marketdb'
+
+
+@app.route('/market')
+def market():
+    item = [
+        {"id":1, "name":"vita marie", "barcode":10202020, "price":500},
+        {"id":1, "name":"Born borm", "barcode":10202020, "price":500},
+        {"id":1, "name":"Oreo", "barcode":10202020, "price":500}
+    ]
+    # db = SQLAlchemy(app)
+    # db.create_all(item)
+    return render_template('market.html',items=item)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# db.create_all()
+if __name__ =='__main__':
+    app.run(debug=True)
